468c4b31d84f2a016e5b3f3efd0ee11343b69ac4
zaqha <83057869+zaqha@users.noreply.github.com>
2021-06-13 21:16:27 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+echo 'Hello World';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <title>Document</title>
+</head>
+
+<body>
+<img src="./images/pic_trulli.jpg" alt="">
+
+
+
+    <h1>This is heading 1</h1>
+    <h2>This is heading 2</h2>
+    <h3>This is heading 3</h3>
+    <h4>This is heading 4</h4>
+    <h5>This is heading 5</h5>
+    <h6>This is heading 6</h6>
+
+
+    <table>
+
+        <form action="/action_page.php">
+            <label for="fname">First name:</label><br>
+            <input type="text" id="fname" name="fname" value="John"><br>
+            <label for="lname">Last name:</label><br>
+            <input type="text" id="lname" name="lname" value="Doe"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+
+
+        <tr></tr>
+        <td>miu</td>
+        <td>miu</td>
+        <td>miu</td>
+        <td>miu</td>
+        </tr>
+        <tr>
+            <td>a</td>
+            <td>a</td>
+            <td>a</td>
+            <td>a</td>
+        </tr>
+        <tr>
+            <td>rrrr</td>
+            <td>rrrr</td>
+            <td>rrrr</td>
+            <td>rrrr</td>
+        </tr>
+        <tr>
+            <td>33</td>
+            <td>33</td>
+            <td>33</td>
+            <td>33</td>
+        </tr>
+        <tr>
+            <td>5555</td>
+            <td>5555</td>
+            <td>5555</td>
+            <td>5555</td>
+        </tr>
+    </table>
+
+    <ol>
+        <li>rr</li>
+        <li>gg</li>
+        <li>ee</li>
+    </ol>
+
+
+    <ul>
+        <li>456</li>
+        <li>789</li>
+        <li>7889</li>
+    </ul>
+</body>
+
+</html>
